fa444cad02e0920c030e545ddabca6bf6446ad2c
ZayoudAhmad <zayoud.ahmed@gmail.com>
2024-10-20 15:58:14 +0100

    defined some constants and created the Bird object with the jumb and move functions

---
+++ Bird.py
@@ -0,0 +1,28 @@
+import pygame
+from flappy_bird import *
+class Bird:
+
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2 #-10.5 + 1.5 = -9
