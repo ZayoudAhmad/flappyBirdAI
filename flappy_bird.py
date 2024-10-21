8acc06c71dffeaa926407fe22f408dc3b04808be
ZayoudAhmad <zayoud.ahmed@gmail.com>
2024-10-21 09:39:51 +0100

    finished the draw method in Bird and created the main method and window draw method in the main file

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,15 +3,32 @@ import neat
 import time
 import os
 import random #for randomly placing the height of the tubes
+from Bird import Bird
 
 #setting the dimension of my screen
-WIN_WIDTH = 600
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
-BIRD_IMGS = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))
-]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0,0))
+    bird.draw(win)
+    pygame.display.update()
+
+def main():
+    bird = Bird(200,200)
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window(win, bird)
+
+    pygame.quit()
+    quit()
+
+main()
