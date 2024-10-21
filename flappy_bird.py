7c15e444e752de9dbfcb6a76ff8a49eae41befba
ZayoudAhmad <zayoud.ahmed@gmail.com>
2024-10-21 11:44:48 +0100

    created the pipe and the base classes, defined their draw and move methods and created the collide method for the pipe

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,31 +3,46 @@ import neat
 import time
 import os
 import random #for randomly placing the height of the tubes
+
 from Bird import Bird
+from Pipe import Pipe
+from Base import Base
 
 #setting the dimension of my screen
-WIN_WIDTH = 500
-WIN_HEIGHT = 800
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
-BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+WIN_WIDTH = 500
+WIN_HEIGHT = 750
 
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base):
     win.blit(BG_IMG, (0,0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    base.draw(win)
+
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird = Bird(200,200)
+    bird = Bird(WIN_WIDTH // 2 - Bird.IMGS[0].get_width() // 2, WIN_HEIGHT // 2 - Bird.IMGS[0].get_height() // 2)
+    base = Base(WIN_HEIGHT-Base.IMG.get_height() // 3)
+    pipes = [Pipe(WIN_WIDTH*1.5)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+    pipe = Pipe(WIN_WIDTH)
 
     run = True
     while run:
+        clock.tick(25)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window(win, bird)
-
+        #bird.move()
+        base.move()
+        for pipe in pipes:
+            pipe.move()
+        draw_window(win, bird, pipes, base)
     pygame.quit()
     quit()
 
